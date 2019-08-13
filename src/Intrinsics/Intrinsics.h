cd90a7689becadd28349115dcd6646a409cff704
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-08-13 08:09:20 +0100

    Use CPU_FEATURES define to try and fix arm build

--- src/Intrinsics/Intrinsics.h
+++ src/Intrinsics/Intrinsics.h
@@ -1,8 +1,14 @@
-#if defined(__x86_64__) || (defined(_MSC_VER) && defined(_WIN64))
+#include "cpu_features/include/cpu_features_cache_info.h"
+
+#if defined(CPU_FEATURES_ARCH_X86_64)
+
+#warning "Using X86_64 Blake AVX2 Intrinsics"
 
 #include "Intrinsics/X86/BlakeIntrinsics.h"
 
 #else
+
+#warning "Using cross platform Blake code"
 
 #include "Intrinsics/CrossPlatform/BlakeIntrinsics.h"
 
