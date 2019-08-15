7b49817aa3e3c44f7856a30ed7184bce98a1c65c
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-08-15 05:53:03 +0100

    Inline rotations

--- src/Intrinsics/X86/ArgonIntrinsics.h
+++ src/Intrinsics/X86/ArgonIntrinsics.h
@@ -12,3 +12,19 @@
 
 static const cpu_features::X86Features features = cpu_features::GetX86Info().features;
 static const bool hasAVX2 = features.avx2;
+
+void blamkaG1AVX2(
+    __m256i& a0, __m256i& a1, __m256i& b0, __m256i& b1,
+    __m256i& c0, __m256i& c1, __m256i& d0, __m256i& d1);
+
+void blamkaG2AVX2(
+    __m256i& a0, __m256i& a1, __m256i& b0, __m256i& b1,
+    __m256i& c0, __m256i& c1, __m256i& d0, __m256i& d1);
+
+void diagonalizeAVX2v1(__m256i& b0, __m256i& c0, __m256i& d0, __m256i& b1, __m256i& c1, __m256i& d1);
+
+void diagonalizeAVX2v2(__m256i& b0, __m256i& b1, __m256i& c0, __m256i& c1, __m256i& d0, __m256i& d1);
+
+void undiagonalizeAVX2v1(__m256i& b0, __m256i& c0, __m256i& d0, __m256i& b1, __m256i& c1, __m256i& d1);
+
+void undiagonalizeAVX2v2(__m256i& b0, __m256i& b1, __m256i& c0, __m256i& c1, __m256i& d0, __m256i& d1);
--- src/Intrinsics/X86/Rotations.h
+++ src/Intrinsics/X86/Rotations.h
@@ -2,14 +2,51 @@
 //
 // Please see the included LICENSE file for more information.
 
+// Copyright (c) 2017, YANDEX LLC
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+// that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and 
+// the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and 
+// the following disclaimer in the documentation and/or other materials provided with the distribution.
+// 
+// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote 
+// products derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #pragma once
 
 #include "Intrinsics/X86/IncludeIntrinsics.h"
 
-__m256i rotr32(__m256i x);
+inline __m256i rotr32(__m256i x) {
+    return _mm256_shuffle_epi32(x, _MM_SHUFFLE(2, 3, 0, 1));
+}
 
-__m256i rotr24(__m256i x);
+inline __m256i rotr24(__m256i x) {
+    return _mm256_shuffle_epi8(x, _mm256_setr_epi8(
+        3, 4, 5, 6, 7, 0, 1, 2, 11, 12, 13, 14, 15, 8, 9, 10,
+        3, 4, 5, 6, 7, 0, 1, 2, 11, 12, 13, 14, 15, 8, 9, 10
+    ));
+}
 
-__m256i rotr16(__m256i x);
+inline __m256i rotr16(__m256i x) {
+    return _mm256_shuffle_epi8(x, _mm256_setr_epi8(
+        2, 3, 4, 5, 6, 7, 0, 1, 10, 11, 12, 13, 14, 15, 8, 9,
+        2, 3, 4, 5, 6, 7, 0, 1, 10, 11, 12, 13, 14, 15, 8, 9
+    ));
+}
 
-__m256i rotr63(__m256i x);
+inline __m256i rotr63(__m256i x) {
+    return _mm256_xor_si256(_mm256_srli_epi64(x, 63), _mm256_add_epi64(x, x));
+}
