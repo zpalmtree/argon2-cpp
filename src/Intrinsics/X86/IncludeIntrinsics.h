d8504c09772e2a7dec8dc5e73c655321047f56e6
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-09-14 23:11:39 +0100

    Debug info

--- src/Intrinsics/X86/IncludeIntrinsics.h
+++ src/Intrinsics/X86/IncludeIntrinsics.h
@@ -5,12 +5,15 @@
 #pragma once
 
 #if defined(_MSC_VER)
+    #pragma message("Including <intrin.h>")
     /* Microsoft C/C++-compatible compiler */
     #include <intrin.h>
 #elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
+    #pragma message("Including <x86intrin.h>")
     /* GCC-compatible compiler, targeting x86/x86-64 */
     #include <x86intrin.h>
 #elif defined(__GNUC__) && defined(__IWMMXT__)
+    #pragma message("Including <mmintrin.h>")
     /* GCC-compatible compiler, targeting ARM with WMMX */
     #include <mmintrin.h>
 #else
