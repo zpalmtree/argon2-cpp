3ba37100d430311ebe4a3ff7959c7a1f18c6e296
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-09-22 05:11:27 +0100

    Remove debug info

--- src/Intrinsics/X86/IncludeIntrinsics.h
+++ src/Intrinsics/X86/IncludeIntrinsics.h
@@ -4,16 +4,6 @@
 
 #pragma once
 
-#if __has_include(<immintrin.h>)
-    #pragma message("Have <immintrin.h>")
-    #include <immintrin.h>
-#endif
-
-#if __has_include(<x86intrin.h>)
-    #pragma message("Have <x86intrin.h>")
-    #include <x86intrin.h>
-#endif
-
 #if defined(_MSC_VER)
     /* Microsoft C/C++-compatible compiler */
     #include <intrin.h>
