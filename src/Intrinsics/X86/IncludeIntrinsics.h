7bc25f769240480d51bbd24024e4aefb666922bc
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-09-22 21:09:46 +0100

    Remove debug statements

--- src/Intrinsics/X86/IncludeIntrinsics.h
+++ src/Intrinsics/X86/IncludeIntrinsics.h
@@ -9,11 +9,9 @@
     /* Microsoft C/C++-compatible compiler */
     #include <intrin.h>
 #elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
-    #pragma message("Including <x86intrin.h>")
     /* GCC-compatible compiler, targeting x86/x86-64 */
     #include <x86intrin.h>
 #elif defined(__GNUC__) && defined(__IWMMXT__)
-    #pragma message("Including <mmintrin.h>")
     /* GCC-compatible compiler, targeting ARM with WMMX */
     #include <mmintrin.h>
 #else
